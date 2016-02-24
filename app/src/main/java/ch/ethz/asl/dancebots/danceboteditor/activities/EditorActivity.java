431d4f640314f105e0f80bf4d5d02e67b6628ede
ajenal <ajenal@student.ethz.ch>
2016-02-24 08:48:43 +0100

    Refactoring

--- app/src/main/java/ch/ethz/asl/dancebots/danceboteditor/activities/EditorActivity.java
+++ app/src/main/java/ch/ethz/asl/dancebots/danceboteditor/activities/EditorActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
